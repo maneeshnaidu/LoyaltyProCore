2291e60b92f7bf1e51dec0faf0e03a133aff9b57
maneeshnaidu <maneeshnjr7@gmail.com>
2024-11-03 13:50:54 +1300

    Updated swagger for prod

--- src/LoyaltyProCore/Program.cs
+++ src/LoyaltyProCore/Program.cs
@@ -65,7 +65,7 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
