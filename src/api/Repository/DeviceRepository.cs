02a6dce582fe48fbc4b9d745ac8a30c48647e4d0
maneeshnaidu <maneeshnjr7@gmail.com>
2025-03-30 11:18:37 +1300

    Added device repo

--- src/api/Repository/DeviceRepository.cs
+++ src/api/Repository/DeviceRepository.cs
@@ -3,6 +3,7 @@ using api.Interfaces;
 using api.Data;
 using api.Dtos.Device;
 using api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Repository
 {
@@ -13,37 +14,71 @@ namespace api.Repository
         {
             _context = context;
         }
-        public Task<Device> CreateAsync(Device model)
+        public async Task<Device> CreateAsync(Device model)
         {
-            // await _context.Devices.AddAsync(model);
-            // await _context.SaveChangesAsync();
-            // return model;
-            throw new NotImplementedException();
+            await _context.Devices.AddAsync(model);
+            await _context.SaveChangesAsync();
+            return model;
         }
 
-        public Task<Device?> DeleteAsync(int id)
+        public async Task<Device?> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var deviceModel = await _context.Devices.FirstOrDefaultAsync(x => x.Id == id);
+            if (deviceModel == null)
+            {
+                return null;
+            }
+            _context.Devices.Remove(deviceModel);
+            await _context.SaveChangesAsync();
+            return deviceModel;
         }
 
         public Task<bool> DeviceExists(int id)
         {
-            throw new NotImplementedException();
+            return _context.Devices.AnyAsync(x => x.Id == id);
         }
 
-        public Task<List<Device>> GetAllAsync(QueryObject query)
+        public async Task<List<Device>> GetAllAsync(QueryObject query)
         {
-            throw new NotImplementedException();
+            var devices = _context.Devices.AsQueryable();
+
+            // if (!string.IsNullOrWhiteSpace(query.CompanyName))
+            // {
+            //     devices = devices.Where(x => x.Name.Contains(query.CompanyName));
+            // }
+
+            // if (!string.IsNullOrWhiteSpace(query.SortBy))
+            // {
+            //     if (query.SortBy.Equals("Name", StringComparison.OrdinalIgnoreCase))
+            //     {
+            //         devices = query.IsDecsending ? devices.OrderByDescending(v => v.Name) : devices.OrderBy(v => v.Name);
+            //     }
+            // }
+
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await devices.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
-        public Task<Device?> GetByIdAsync(int id)
+        public async Task<Device?> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _context.Devices.FirstOrDefaultAsync(x => x.Id == id);
         }
 
-        public Task<Device?> UpdateAsync(int id, UpdateDeviceDto deviceRequestDto)
+        public async Task<Device?> UpdateAsync(int id, UpdateDeviceDto deviceRequestDto)
         {
-            throw new NotImplementedException();
+            var existingDevice = await _context.Devices.FirstOrDefaultAsync(x => x.Id == id);
+            if (existingDevice == null)
+            {
+                return null;
+            }
+            existingDevice.DeviceId = deviceRequestDto.DeviceId;
+            existingDevice.DeviceToken = deviceRequestDto.DeviceToken;
+            existingDevice.DeviceType = deviceRequestDto.DeviceType;
+            existingDevice.IsActive = deviceRequestDto.IsActive;
+
+            await _context.SaveChangesAsync();
+            return existingDevice;
         }
     }
 }
