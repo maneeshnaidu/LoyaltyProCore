f9ec69fa71c42dc0a697690af9d3119e54be439d
maneeshnaidu <maneeshnjr7@gmail.com>
2025-05-17 17:33:46 +1200

    Refactored structure

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -83,7 +83,7 @@ builder.Services.AddAuthentication(options =>
         ValidAudience = builder.Configuration["JWT:Audience"],
         ValidateIssuerSigningKey = true,
         IssuerSigningKey = new SymmetricSecurityKey(
-            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])
+            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"] ?? "")
         )
     };
 });
